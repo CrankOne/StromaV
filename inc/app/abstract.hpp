3c774e5af00bd83fbb3c70575b643614e4a76e72
Crank <crank@qcrypt.org>
2017-07-21 19:50:41 +0700

    ~ futile attempt to make iApp singleton visible among python imports

--- inc/app/abstract.hpp
+++ inc/app/abstract.hpp
@@ -70,9 +70,9 @@ namespace sV {
  *
  * @ingroup app
  */
-class AbstractApplication : public goo::App<goo::dict::Configuration, std::ostream>,
-                            public sV::logging::Logger,
-                            public sV::aux::ASCII_Display {
+class DLL_PUBLIC AbstractApplication : public goo::App<goo::dict::Configuration, std::ostream>,
+                                       public sV::logging::Logger,
+                                       public sV::aux::ASCII_Display {
 public:
     /// A particular config class type. Aggregates configuration parameters
     /// into the annotated dictionary. StromaV now uses
